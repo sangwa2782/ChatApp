5f0c338fc2e471d74e509c10a8fa2e0ad7ee723b
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-10-01 09:07:39 +0530

    Initial commit

---
+++ app/src/main/java/com/ajayam/chatapp/LoginOtpActivity.java
@@ -0,0 +1,105 @@
+package com.ajayam.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ajayam.chatapp.utils.AndroidUtil;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LoginOtpActivity extends AppCompatActivity {
+
+    String phoneNumber;
+    Long timeoutSeconds = 60L;
+    String varificationCode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+
+    EditText otpInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+
+        otpInput = findViewById(R.id.login_otp);
+        nextBtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+
+        sendOtp(phoneNumber,false);
+
+    }
+
+    void sendOtp(String phoneNumber, boolean isResend) {
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                AndroidUtil.showToast(getApplicationContext(), "OTP varification failed");
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                varificationCode = s;
+                                resendingToken = forceResendingToken;
+                                AndroidUtil.showToast(getApplicationContext(), "OTP sent Successfully");
+                                setInProgress(false);
+                            }
+                        });
+        if (isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        } else {
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
+
+    void setInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    void signIn(PhoneAuthCredential phoneAuthCredential) {
+
+    }
+}
